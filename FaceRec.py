eb523ba78cb637ffe5ce090e11aa591eeaf4227b
ElFeeder <bernardo.galego@tecnico.ulisboa.pt>
2020-07-31 13:44:25 +0100

    Just needs bug fixing

--- FaceRec.py
+++ FaceRec.py
@@ -3,8 +3,8 @@ import os
 import cv2
 
 
-DIR_KNOWN = "known"     # Directories where the faces are
-DIR_UNKNOWN = "unknown"
+DIR_KNOWN = "Known"     # Directories where the faces are
+DIR_UNKNOWN = "Unknown"
 
 tolerance = 0.6         # Tolerance of a match (% of accuracy permitted)
 
@@ -13,23 +13,73 @@ fontThickness = 2
 
 model = "cnn"           # What NN we'll be using
 
-
-print("Loading known faces...")
-
 knownFaces = []         # List of all the different faces that we know
 knownNames = []         # Give a name to each face
 
+
+print("Loading known faces...")
+
 for name in os.listdir(DIR_KNOWN):                      # List of all the names in the Known directory
     for filename in os.listdir(f"{DIR_KNOWN}/{name}"):  # List all files there
+        print("Checking " + filename)
         # Load an image
         image = face_recognition.load_image_file(f"{DIR_KNOWN}/{name}/{filename}")
         
-        # Encode only one face in that image (hence the [0])
-        # Known faces should only have one face in it
-        encoding = face_recognition.face_encodings(image)[0]
+        # If we found exactly one face, use it
+        if(len(face_recognition.face_encodings(image)) == 1):
+            encoding = face_recognition.face_encodings(image)
 
-        # Add the face and the name to the lists
-        knownFaces.append(encoding)
-        knownNames.append(name)
+            # Add the face and the name to the lists
+            knownFaces.append(encoding)
+            knownNames.append(name)
+        elif(len(face_recognition.face_encodings(image)) > 1):
+            print("/tFile " + filename + " had more than one face")
+        elif(len(face_recognition.face_encodings(image)) < 1):
+            print("File " + filename + " had no faces")
 
 
+print("Processing unknown faces...")
+
+for filename in os.listdir(DIR_UNKNOWN):
+    print("Checking " + filename)
+    image = face_recognition.load_image_file(f"{DIR_UNKNOWN}/{filename}")
+
+    # Find every face in each given image and encode each one
+    locations = face_recognition.face_locations(image, model=model)
+    encodings = face_recognition.face_encodings(image, locations)
+
+    # Now to draw on the image, we need to make it workable by CV2
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # For each encoding that we have, compare against encodings of known faces
+    for faceEncoding, faceLocation in zip(encodings, locations):
+        results = face_recognition.compare_faces(knownFaces, faceEncoding, tolerance)
+
+        # If we have a match, the associated name will be in the same position as the face
+        # was in knownFaces
+        if True in results:
+            match = knownNames[results.index(True)]
+            print("Match found: " + match)
+
+            # Draw a rectangle around the face
+            topLeft = (faceLocation[3], faceLocation[0])
+            bottomRight = (faceLocation[1], faceLocation[2])
+
+            # Have a specific color to each face
+            color = [(ord(c.lower())-97)*8 for c in match[:3]]
+
+            cv2.rectangle(image, topLeft, bottomRight, color, frameThickness)
+
+            # Nameplate
+            topLeft = (faceLocation[3], faceLocation[2])
+            bottomRight = (faceLocation[1], faceLocation[2] + 22)
+
+            cv2.rectangle(image, topLeft, bottomRight, color, cv2.FILLED)
+
+            # Name the rectangle
+            cv2.putText(image, match, (faceLocation[3] + 10, faceLocation[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), fontThickness)
+
+    # Show the image until I press something
+    cv2.imshow(filename, image)
+    cv2.waitKey(0)
+    cv2.destroyWindow(filename)
